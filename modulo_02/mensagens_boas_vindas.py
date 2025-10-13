6a8ad9379ed8e1c1a1185e13f4f6b73815506327
Naah <nathaly.s.ferreira2007@gmail.com>
2025-10-13 17:07:19 -0300

    Add files via upload

---
+++ modulo_02/mensagens_boas_vindas.py
@@ -0,0 +1,28 @@
+'''
+
+nomeclatura
+PASCALCase
+CamelCase
+-> Snake_Case <-
+Kebab-Case
+
+Comentários, pode ser usados como apostrofos ou cerquilhas
+
+
+'''
+
+#python 3.14
+
+print("\n:D:D:D:D:D:D:D:D:D:D")
+print("\fbem vindo")
+print("\n:D:D:D:D:D:D:D:D:D:D")
+
+nome_pessoa = str(input("Qual é o seu nome? "))
+
+idade_pessoa = int(input("Qual é a sua idade? "))
+
+altura_pessoa = float(input("Qual é a sua altura? "))
+
+print(f"Olá, {nome_pessoa}! sua idade é {idade_pessoa}")
+print(f"sua altura é {altura_pessoa}")
+print(f"Seja bem vindo! :D")
