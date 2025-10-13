a1117445e6b5c69d7652c9704700853de7e283ea
Naah <nathaly.s.ferreira2007@gmail.com>
2025-10-13 14:33:48 -0300

    hello world
    hello world

---
+++ modulo_01/hello_world.py
@@ -0,0 +1,18 @@
+'''
+
+nomeclatura
+PASCALCase
+CamelCase
+-> Snake_Case <-
+Kebab-Case
+
+Comentários, pode ser usados como apostrofos ou cerquilhas
+
+
+'''
+# Exibe a mensagem na tela
+#print ('Hello World')
+print ('Hello World')
+
+# pede para o usuario digitar o nome
+input("muito bem, qual é o seu nome?") 
